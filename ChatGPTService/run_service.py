4f817d061f408ba5a72f6a33147fef23549678e0
AliceWorkSpace <alice.pro.life@qq.com>
2023-04-03 14:49:20 +0800

    项目功能初始化

---
+++ ChatGPTService/run_service.py
@@ -0,0 +1,21 @@
+# coding: utf8
+""" 
+@File: run_service.py
+@Editor: PyCharm
+@Author: Alice(From Chengdu.China)
+@HomePage: https://github.com/AliceEngineerPro
+@OS: Windows 11 Professional Workstation 22H2
+@Environment: Python3.9 (FairyAdministrator)
+@CreatedTime: 2023/4/3 12:38
+"""
+
+
+from core import bot
+from utils import logs
+
+def run_service():
+    logs.info('开始运行...')
+    
+    
+if __name__ == '__main__':
+    run_service()
